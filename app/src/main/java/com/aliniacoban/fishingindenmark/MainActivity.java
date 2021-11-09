180203b3feb106018278595a05a8d06c18b67df1
alinusiacoban <aliniacoban@yahoo.com>
2021-11-09 03:00:35 +0100

    Update with bottom Navigation.

--- app/src/main/java/com/aliniacoban/fishingindenmark/MainActivity.java
+++ app/src/main/java/com/aliniacoban/fishingindenmark/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                     if (user.isEmailVerified()) {
                         //redirect to user profile
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                        startActivity(new Intent(MainActivity.this, MainPage.class));
                     } else {
                         user.sendEmailVerification();
                         Toast.makeText(MainActivity.this, "Check your email to verify your account", Toast.LENGTH_LONG).show();
